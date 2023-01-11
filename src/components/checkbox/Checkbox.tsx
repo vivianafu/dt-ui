d05fde118d2659a505b41905c5d324451fab266d
WHQ\11108802 <viviana_fu@wistron.com>
2023-01-11 16:02:50 +0800

    feat(checkbox): add checkbox component and related stories

---
+++ src/components/checkbox/Checkbox.tsx
@@ -0,0 +1,56 @@
+import { useId, forwardRef } from 'react';
+
+import clsx from 'clsx';
+
+import type { Variants } from './types';
+import type { ChangeEventHandler } from 'react';
+
+type Props = {
+  label?: string;
+  splitter?: string;
+  disabled?: boolean;
+  defaultChecked?: boolean;
+  className?: string;
+  variant: Variants;
+  onChange?: ChangeEventHandler<HTMLInputElement>;
+};
+
+const variantStyles: { [K in Variants]: string } = {
+  default:
+    'rounded border-gray-50 bg-transparent text-primary-500 focus:ring-primary-600 focus:ring-offset-primary-900 dark:text-cyan-500 dark:focus:ring-cyan-600 dark:focus:ring-offset-cyan-900',
+  unstyled: '',
+};
+
+const Checkbox = forwardRef(
+  (
+    { disabled = false, onChange, defaultChecked, label = '', variant = 'default', className = 'first-letter:' }: Props,
+    ref
+  ) => {
+    const id = useId();
+
+    return (
+      <div className={clsx(label ? 'flex items-center' : '')}>
+        <input
+          ref={() => ref && (ref as React.MutableRefObject<unknown>).current}
+          type="checkbox"
+          id={id}
+          className={clsx(
+            variant ? variantStyles[variant] : '',
+            disabled ? 'pointer-events-none !border-opacity-50 text-gray-600' : '',
+            className
+          )}
+          disabled={disabled}
+          defaultChecked={defaultChecked}
+          onChange={onChange}
+        />
+        {label && (
+          <label htmlFor={id} className={clsx('ml-1.5 inline-flex whitespace-nowrap text-gray-50')}>
+            {label}
+          </label>
+        )}
+      </div>
+    );
+  }
+);
+
+export default Checkbox;
