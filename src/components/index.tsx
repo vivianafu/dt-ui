9f68bda30cee1092def742b68e98dc5d1e57431d
WHQ\11108802 <viviana_fu@wistron.com>
2023-01-13 15:40:28 +0800

    build: intergrate export component files

---
+++ src/components/index.tsx
@@ -0,0 +1,7 @@
+export { default as Button } from './button'
+export { default as Checkbox } from './checkbox'
+export { default as Input } from './input'
+export { default as Modal } from './modal'
+export { default as Select } from './select'
+export { default as Toggle } from './toggle'
+export { default as Tooltip } from './tooltip'
