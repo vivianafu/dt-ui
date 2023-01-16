8a5db5f05fbba8d5e0b6ba9381c26162efb390db
WHQ\11108802 <viviana_fu@wistron.com>
2023-01-16 14:16:20 +0800

    edit(checkbox): edit checkbox props

--- src/components/checkbox/Checkbox.tsx
+++ src/components/checkbox/Checkbox.tsx
@@ -1,35 +1,46 @@
-import { useId, forwardRef } from 'react';
+import { useId, forwardRef } from 'react'
 
-import clsx from 'clsx';
+import clsx from 'clsx'
 
-import type { Variants } from './types';
-import type { ChangeEventHandler } from 'react';
+import type { Variants } from './types'
+import type { ChangeEventHandler } from 'react'
 
 type Props = {
-  label?: string;
-  splitter?: string;
-  disabled?: boolean;
-  defaultChecked?: boolean;
-  className?: string;
-  variant?: Variants;
-  onChange?: ChangeEventHandler<HTMLInputElement>;
-};
+  label?: string
+  splitter?: string
+  disabled?: boolean
+  defaultChecked?: boolean
+  className?: string
+  inputClassName?: string
+  labelClassName?: string
+  variant?: Variants
+  onChange?: ChangeEventHandler<HTMLInputElement>
+}
 
 const variantStyles: { [K in Variants]: string } = {
   default:
     'rounded border-gray-50 bg-transparent text-primary-500 focus:ring-primary-600 focus:ring-offset-primary-900 dark:text-cyan-500 dark:focus:ring-cyan-600 dark:focus:ring-offset-cyan-900',
   unstyled: '',
-};
+}
 
 const Checkbox = forwardRef(
   (
-    { disabled = false, onChange, defaultChecked, label = '', variant = 'default', className = 'first-letter:' }: Props,
-    ref
+    {
+      disabled = false,
+      onChange,
+      defaultChecked,
+      label = '',
+      variant = 'default',
+      className = '',
+      inputClassName = '',
+      labelClassName = '',
+    }: Props,
+    ref,
   ) => {
-    const id = useId();
+    const id = useId()
 
     return (
-      <div className={clsx(label ? 'flex items-center' : '')}>
+      <div className={clsx(label ? 'inline-flex items-center' : '', className)}>
         <input
           ref={() => ref && (ref as React.MutableRefObject<unknown>).current}
           type="checkbox"
@@ -37,20 +48,20 @@ const Checkbox = forwardRef(
           className={clsx(
             variant ? variantStyles[variant] : '',
             disabled ? 'pointer-events-none !border-opacity-50 text-gray-600' : '',
-            className
+            inputClassName,
           )}
           disabled={disabled}
           defaultChecked={defaultChecked}
           onChange={onChange}
         />
         {label && (
-          <label htmlFor={id} className={clsx('ml-1.5 inline-flex whitespace-nowrap text-gray-50')}>
+          <label htmlFor={id} className={clsx('ml-1.5 inline-flex whitespace-nowrap text-gray-50', labelClassName)}>
             {label}
           </label>
         )}
       </div>
-    );
-  }
-);
+    )
+  },
+)
 
-export default Checkbox;
+export default Checkbox
