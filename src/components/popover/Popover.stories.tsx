268b1ec3bb2eca223f954eaf1e4cfd91e26846da
Vivian Fu <traveler00035@gmail.com>
2023-02-02 15:15:01 +0800

    feat(popover): add placementTop story

--- src/components/popover/Popover.stories.tsx
+++ src/components/popover/Popover.stories.tsx
@@ -2,8 +2,21 @@ import Popover from './Popover';
 
 import type { Meta, StoryObj } from '@storybook/react';
 
-const meta: Meta<typeof Popover> = { title: 'Components/Popover', component: Popover, argTypes: {} };
+const meta: Meta<typeof Popover> = {
+  title: 'Components/Popover',
+  component: Popover,
+  argTypes: {},
+  parameters: { layout: 'centered' },
+};
 export default meta;
 
 type Story = StoryObj<typeof Popover>;
-export const Basic: Story = { render: () => <Popover render={<>hello</>}>Trigger</Popover> };
+export const Default: Story = { render: () => <Popover render={<>hello</>}>Trigger</Popover> };
+
+export const PlacementTop: Story = {
+  render: () => (
+    <Popover render={<>hello</>} placement="top">
+      PlacementTop
+    </Popover>
+  ),
+};
